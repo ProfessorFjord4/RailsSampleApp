70d9d2d7d9ade9084b8578a56096338e52267f3e
Irie Mahiro <iriemahiro@IrienoMacBook-Air.local>
2024-04-07 19:04:26 +0900

    ユーザ認証機能の実装

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+end
+OmniAuth.config.allowed_request_methods = [:post, :get]
